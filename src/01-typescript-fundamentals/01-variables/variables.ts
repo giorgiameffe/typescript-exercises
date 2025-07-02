dac814799fb8540f0dba669b7d27ec43996d603a
giorgiameffe <giorgiameffe03@gmail.com>
2025-07-02 15:42:43 +0200

    feat: add typescript exercises and complete string variable exercise

---
+++ src/01-typescript-fundamentals/01-variables/variables.ts
@@ -0,0 +1,42 @@
+// ✏️ Esercizio 1 – Variabile stringa
+
+// Dichiara una variabile name e assegnale una stringa con il tuo nome.
+
+const myName: string = 'Giorgia';
+console.log(myName);
+
+// ✏️ Esercizio 2 – Variabile numerica
+
+// Dichiara una variabile age e assegnale un numero intero.
+
+// ✏️ Esercizio 3 – Costante
+
+// Crea una costante PI con valore 3.14.
+
+// ✏️ Esercizio 4 – Variabile booleana
+
+// Dichiara una variabile isOnline e assegnale il valore true.
+
+// ✏️ Esercizio 5 – Tipizzazione implicita
+
+// Dichiara una variabile colorePreferito senza specificare il tipo, ma assegnale un valore stringa.
+
+// ✏️ Esercizio 6 – Riassegnazione
+
+// Crea una variabile punteggio con valore 0, poi aggiornala a 100.
+
+// ✏️ Esercizio 7 – Somma di due numeri
+
+// Dichiara due variabili a e b, sommale e stampa il risultato.
+
+// ✏️ Esercizio 8 – String template
+
+// Crea una variabile nomeUtente, una anni, e stampa "Ciao Marco, hai 28 anni." usando template string.
+
+// ✏️ Esercizio 9 – Errore di tipo
+
+// Dichiara una variabile livello come number e prova ad assegnarle una stringa.
+
+// ✏️ Esercizio 10 – Variabili multiple
+
+// Dichiara tre variabili in una sola riga: x = 5, y = 10, z = 15, e stampale.
