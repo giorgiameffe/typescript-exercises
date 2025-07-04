2a8a541f9ec33412069518de862e24651599e95a
giorgiameffe <giorgiameffe03@gmail.com>
2025-07-04 10:31:22 +0200

    feat: complete exercise one special types (any)

---
+++ src/01-typescript-fundamentals/03-special-types/specialTypes.ts
@@ -0,0 +1,11 @@
+// ✏️ Esercizio 1 – Any
+// Dichiara una variabile value di tipo any e assegnale un numero, poi una stringa, poi un boolean.
+
+let value: any = 30;
+console.log(value);
+
+value = 'Matita';
+console.log(value);
+
+value = true;
+console.log(value);
