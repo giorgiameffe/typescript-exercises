7fb58c7d57d16526e38eb530131196084ff6c4e5
giorgiameffe <giorgiameffe03@gmail.com>
2025-07-07 21:27:18 +0200

    feat: update package.json and complete exercise one (typed functions)

---
+++ src/typescript-base/06-typed-functions/exercises.ts
@@ -0,0 +1,34 @@
+// ✏️ Esercizio 1: Somma
+// Scrivi una funzione che accetta due numeri e restituisce la loro somma.
+
+{
+    function sum(num1: number, num2: number): number {
+
+        return num1 + num2;
+    }
+
+    console.log(sum(7, 9));
+}
+
+// ✏️ Esercizio 2: Crea saluto
+// Scrivi una funzione che prende un nome (stringa) e ritorna una frase tipo: "Ciao, Mario!".
+
+
+
+// ✏️ Esercizio 3: Controlla maggiore età
+// Scrivi una funzione che prende un'età (numero) e restituisce true se è maggiore o uguale a 18, altrimenti false.
+
+
+
+
+// ✏️ Esercizio 4: Array di nomi
+// Scrivi una funzione che accetta un array di stringhe e stampa ogni nome.
+
+
+
+
+// ✏️ Esercizio 5: Funzione con oggetto
+// Scrivi una funzione che prende un oggetto { nome: string; eta: number } e ritorna una stringa descrittiva.
+
+
+
---
+++ src/typescript-base/07-union-literal-types/exercises.ts
@@ -0,0 +1,29 @@
+// ✏️ Esercizio 1: Union type con parametri
+// Crea una funzione stampaValore che accetta un parametro che può essere stringa o numero e lo stampa.
+
+
+// ✏️ Esercizio 2: Literal type semplice
+// Definisci una variabile stato che può essere solo "online" o "offline".
+
+
+// ✏️ Esercizio 3: Funzione con literal type
+// Scrivi una funzione segnalaStato che accetta solo "ok" o "errore" e stampa un messaggio diverso in base al valore.
+
+
+
+// ✏️ Esercizio 4: Uso di type con Union e Literal Types
+// 1. Definisci un type chiamato RuoloUtente, che può essere solo uno di questi valori letterali:
+// "admin", "user", "guest".
+// 2. Crea un type chiamato Utente, con le seguenti proprietà:
+// nome: string, ruolo: RuoloUtente
+// 3. Scrivi una funzione verificaAccesso che accetta un oggetto di tipo Utente
+// e stampa un messaggio diverso a seconda del ruolo.
+
+
+
+
+// ✏️ Esercizio 5: Funzione con union numerico e stringa
+// Crea una funzione calcolaSconto che accetta o:
+// un numero (percentuale)
+// o la stringa "nessuno"
+// Se è un numero, calcola lo sconto su un prezzo. Se "nessuno", torna il prezzo intero.
