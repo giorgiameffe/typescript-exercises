e412eab55aa99b1f4804483db4c30d92d9e5a100
giorgiameffe <giorgiameffe03@gmail.com>
2025-07-07 16:16:28 +0200

    feat: update package.json and complete exercise one (type)

---
+++ src/typescript-base/05-type-interface/exercises.ts
@@ -0,0 +1,11 @@
+// ✏️ Esercizio 1: type base
+// Crea un type chiamato Product con queste proprietà: name e price
+// Crea una variabile di tipo Product e stampala.
+
+type Product = {
+    name: string,
+    price: number
+}
+
+const product: Product = { name: 'Tablet', price: 100 };
+console.log(product);
